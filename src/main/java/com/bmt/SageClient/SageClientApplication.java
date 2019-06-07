d92df10d6baff824724d391ec97dc0453a4f32ab
bmt94 <daleel94@live.co.uk>
2019-06-07 02:51:43 +0100

    Base project, initial commit

---
+++ src/main/java/com/bmt/SageClient/SageClientApplication.java
@@ -0,0 +1,13 @@
+package com.bmt.SageClient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SageClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SageClientApplication.class, args);
+	}
+
+}
