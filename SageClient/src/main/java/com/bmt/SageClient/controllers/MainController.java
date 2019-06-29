62365c71f244fdd4d0ae53fb9bffa8231eb677d7
bmt94 <daleel94@live.co.uk>
2019-06-29 03:29:03 +0100

    front end can make api call to server

--- SageClient/src/main/java/com/bmt/SageClient/controllers/MainController.java
+++ SageClient/src/main/java/com/bmt/SageClient/controllers/MainController.java
@@ -3,12 +3,14 @@ package com.bmt.SageClient.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bmt.SageClient.orm.dao.SageAPIHandlerDAO;
 import com.bmt.SageClient.sage200api.entities.CustomerViews;
 
+@CrossOrigin
 @RestController
 public class MainController 
 {
--- sage-client-frontend/app/components/test/testcontroller.js
+++ sage-client-frontend/app/components/test/testcontroller.js
@@ -1,10 +1,24 @@
 (function() {
 
-    var TestController =  function() {
+    var TestController =  function($http) {
         
     	let data = "test data";
-            
+			
+		
+		this.getCustomerViews = () => 
+		{
+			let vm = this;
+			$http.get("http://localhost:8080/customerViews")
+			.then(function(response) {
+				vm.data = response.data;
+				console.log(response);
+			});
+		}
+
+
+		console.log("Test Controller");
+		this.getCustomerViews();
     };
 
-    angular.module('sageClientApp').controller('testController', [TestController]);
+    angular.module('sageClientApp').controller('testController', ['$http', TestController]);
 }());
